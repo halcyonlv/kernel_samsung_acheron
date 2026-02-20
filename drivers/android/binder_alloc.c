33538bdb94bd447991e991d05ccd7b4c8a97910e
Samuel Pascua <pascua.samuel.14@gmail.com>
2026-02-20 15:34:14 +0800

    android: freecess: Sync with samsung_sm8450 kernel
    Change-Id: I112fc1fdad8e743894402c328cc2f514161feedf
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- drivers/android/binder_alloc.c
+++ drivers/android/binder_alloc.c
@@ -32,6 +32,7 @@
 #include <linux/highmem.h>
 #ifdef CONFIG_SAMSUNG_FREECESS
 #include <linux/freecess.h>
+#include <linux/sched/jobctl.h>
 #endif
 
 #include "binder_alloc.h"
@@ -438,13 +439,12 @@ static struct binder_buffer *binder_alloc_new_buf_locked(
 
 #ifdef CONFIG_SAMSUNG_FREECESS
 	if (is_async && (alloc->free_async_space < 3*(size + sizeof(struct binder_buffer))
-		|| (alloc->free_async_space < ((alloc->buffer_size/2)*9/10)))) {
+		|| (alloc->free_async_space < alloc->buffer_size / 4))) {
 		rcu_read_lock();
 		p = find_task_by_vpid(alloc->pid);
 		rcu_read_unlock();
-		if (p != NULL && thread_group_is_frozen(p)) {
+		if (p && (thread_group_is_frozen(p) || p->jobctl & JOBCTL_TRAP_FREEZE))
 			binder_report(p, -1, "free_buffer_full", is_async);
-		}
 	}
 #endif
 
